5876c649d1387b79f22274abeadc838674c08fe0
anytodo292 <anything.todo292@gmail.com>
2026-09-01 23:57:31 +0100

    fix: made changes

---
+++ src/constants.ts
@@ -0,0 +1,91 @@
+export const en_person_stat = `${__PUBLIC_URL__}/assets/images/emma_en.png`;
+export const en_person_anim = `${__PUBLIC_URL__}/assets/images/emma_en.gif`;
+export const en_person_name = 'Emma'
+
+
+export const InterviewType = {
+  // QUICK_CALL: -1,
+  TECH_INTERVIEW: 0,
+  // HR_INTERVIEW: 1,
+  TEAM_MEETING: 2,
+  CLIENT_MEETING: 3,
+  CONSULTING: 4,
+  CASUAL_CONVERSATION: 5,
+  // ONLINE_ASSESSMENT: 6,
+
+  SCREENING_INTERVIEW: 7,
+  CODING_INTERVIEW: 8,
+  SYS_DESGIN_INTERVIEW: 9,
+  BEHAV_INTERVIEW: 10,
+  CASE_INTERVIEW: 11,
+  SITUATION_INTERVIEW: 12,
+  CULTURE_INTERVIEW: 13,
+  FINAL_INTERVIEW: 14,
+  AI_INTERVIEW: 15,
+};
+
+export const LangType = {
+  CHINESE: 0,
+  DUTCH: 1,
+  ENGLISH: 2,
+  FRENCH: 3,
+  GERMAN: 4,
+  ITALIAN: 5,
+  JAPANESE: 6,
+  SPANISH: 7,
+  RUSSIAN: 8,
+  ARABIC: 9,
+  PORTUGUESE: 10,
+  KOREAN: 11,
+};
+
+export const DifficultyType = {
+  Junior: 0,
+  Mid: 1,
+  Senior: 2,
+};
+
+export const InterviewerInfo = [
+  {
+    language: LangType.ENGLISH,
+    name: 'Emma',
+    anim: `${__PUBLIC_URL__}/assets/images/emma_en.gif`,
+    image: `${__PUBLIC_URL__}/assets/images/emma_en.png`
+  },
+  {
+    language: LangType.DUTCH,
+    name: 'Daan',
+    anim: `${__PUBLIC_URL__}/assets/images/emma_en.gif`,
+    image: `${__PUBLIC_URL__}/assets/images/emma_en.png`
+  },
+  {
+    language: LangType.FRENCH,
+    name: 'Hector',
+    anim: `${__PUBLIC_URL__}/assets/images/emma_en.gif`,
+    image: `${__PUBLIC_URL__}/assets/images/emma_en.png`
+  },
+  {
+    language: LangType.GERMAN,
+    name: 'Fabian',
+    anim: `${__PUBLIC_URL__}/assets/images/emma_en.gif`,
+    image: `${__PUBLIC_URL__}/assets/images/emma_en.png`
+  },
+  {
+    language: LangType.ITALIAN,
+    name: 'Flavio',
+    anim: `${__PUBLIC_URL__}/assets/images/emma_en.gif`,
+    image: `${__PUBLIC_URL__}/assets/images/emma_en.png`
+  },
+  {
+    language: LangType.JAPANESE,
+    name: 'Ebisu',
+    anim: `${__PUBLIC_URL__}/assets/images/emma_en.gif`,
+    image: `${__PUBLIC_URL__}/assets/images/emma_en.png`
+  },
+  {
+    language: LangType.SPANISH,
+    name: 'Nestor',
+    anim: `${__PUBLIC_URL__}/assets/images/emma_en.gif`,
+    image: `${__PUBLIC_URL__}/assets/images/emma_en.png`
+  },
+]
